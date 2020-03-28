572b202f30c04e9579ebe754e88bda09609ccf03
Nelson Baggio <nelson.baggio@totvs.com.br>
2020-03-28 20:24:19 -0300

    Initial producer

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using Confluent.Kafka;
 
 namespace Producer
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+      var message = "1;42";
+      using (var producer = new ProducerBuilder<Null, string>(config).Build())
+      {
         {
-            Console.WriteLine("Hello World!");
+          try
+          {
+            var sendResult = producer
+                                .ProduceAsync("PETSHOP_NEW_ORDER", new Message<Null, string> { Value = message })
+                                    .GetAwaiter()
+                                        .GetResult();
+
+            Console.WriteLine($"Mensagem '{sendResult.Value}' de '{sendResult.TopicPartitionOffset}'");
+          }
+          catch (ProduceException<Null, string> e)
+          {
+            Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+          }
         }
+      }
     }
+  }
 }
